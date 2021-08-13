355b5922196a228514ee95c7cb9af3fa5d14f0b4
GitHub Action <action@github.com>
2021-08-13 06:16:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/briancsparks/lethal/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/briancsparks/lethal/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.briancsparks.lethal.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.briancsparks.lethal.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/briancsparks/lethal/services/MyProjectService.kt
+++ src/main/kotlin/com/github/briancsparks/lethal/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.briancsparks.lethal.services
 
+import com.github.briancsparks.lethal.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
